586087ca459b45db79765e8487d904abae9bd848
Leonardo Nunes <itsmeleo_n@outlook.com>
2022-08-26 09:25:15 -0300

    done

--- js/Transquiz.js
+++ js/Transquiz.js
@@ -19,7 +19,7 @@ class Transquiz {
             Resposta1: "Sim",
             Resposta2: "Não",
             TextoUsuario1: "Legal! Um passo já foi dado. Lembre também que identidade de gênero (como você se identifica) e sexualidade (por quem você se atrai) não possuem ligação!",
-            TextoUsuario2: "Pessoas transgêneras são pessoas que não se identificam com o gênero de registro (nascimento). Pessoas trans podem ser trans binárias (masculino/feminino) ou trans não-binárias (gênero fluido, agênero e mais). Importante: o gênero com que você se identifica NÃO possui ligação com sua sexualidade (pessoa por quem você se atrai)."
+            TextoUsuario2: "Você errou. Pessoas transgêneras são pessoas que não se identificam com o gênero de registro (nascimento). Pessoas trans podem ser trans binárias (masculino/feminino) ou trans não-binárias (gênero fluido, agênero e mais). Importante: o gênero com que você se identifica NÃO possui ligação com sua sexualidade (pessoa por quem você se atrai)."
             },
             {
             Pergunta: "Você contrataria uma pessoa trans para trabalhar na sua empresa?",
@@ -28,7 +28,7 @@ class Transquiz {
             Resposta1: "Sim",
             Resposta2: "Não",
             TextoUsuario1: "Boa! Você faz a diferença. Aproximadamente 38% de empresas tem restrições para a contratação de pessoas LGBTQIA+. Mas não basta apenas contratar, é necessário também o reconhecimento do(a) profissional!",
-            TextoUsuario2: "Infelizmente não são poucas as pessoas que pensam igual você. A taxa de desemprego de pessoas trans no Brasil é de 29,8%, porém 56,82% estão em situação de insegurança alimentar e cerca de 90% das mulheres trans e travestis recorrem à prostituição para conseguirem se manter."
+            TextoUsuario2: "Você errou. Infelizmente não são poucas as pessoas que pensam igual você. A taxa de desemprego de pessoas trans no Brasil é de 29,8%, porém 56,82% estão em situação de insegurança alimentar e cerca de 90% das mulheres trans e travestis recorrem à prostituição para conseguirem se manter."
             },
             {
             Pergunta: "Possui alguma pessoa trans no seu círculo social?",
@@ -37,7 +37,7 @@ class Transquiz {
             Resposta1: "Sim",
             Resposta2: "Não",
             TextoUsuario1: "Gostei de ver! Agora, quantas pessoas? Uma? Mais?",
-            TextoUsuario2: "Te convido a pensar o motivo disso. Você não conhece ou nunca deu abertura à uma pessoa trans?"
+            TextoUsuario2: "Você errou. Te convido a pensar o motivo disso. Você não conhece ou nunca deu abertura à uma pessoa trans?"
             },
             {
             Pergunta: "Aceitaria um filho trans dentro da sua casa?",
@@ -45,8 +45,8 @@ class Transquiz {
             Op2: "Não",
             Resposta1: "Sim",
             Resposta2: "Não",
-            TextoUsuario1: "A maior demonstração de amor que você pode dar à alguém é aceitá-los(as) como eles(as) são. <3",
-            TextoUsuario2: "Das pessoas trans expulsas de casa, 29% tinham 15 anos ou menos e 49% tinham entre 16-20 anos. Em 2019, 35,5% das mortes violentas de LGBTQIA+ aconteceram dentro de suas próprias casas. Não faça parte dessa porcentagem e não deixe sua criança passar por isso."
+            TextoUsuario1: "Isso aí! A maior demonstração de amor que você pode dar à alguém é aceitá-los(as) como eles(as) são. <3",
+            TextoUsuario2: "Você errou. Das pessoas trans expulsas de casa, 29% tinham 15 anos ou menos e 49% tinham entre 16-20 anos. Em 2019, 35,5% das mortes violentas de LGBTQIA+ aconteceram dentro de suas próprias casas. Não faça parte dessa porcentagem e não deixe sua criança passar por isso."
             },
             {
             Pergunta: "Sabia que existem homens que engravidam?",
@@ -55,7 +55,7 @@ class Transquiz {
             Resposta1: "Sim",
             Resposta2: "Não",
             TextoUsuario1: "Parabéns! Poucas pessoas tem a ciência disso.",
-            TextoUsuario2: "Homens trans que possuem útero podem gerar uma criança, porém é necessário a interrupção do tratamento hormonal (testosterona). Podem até amamentar, caso não tenham passado por uma cirurgia de retirada das mamas (mastectomia). Mulheres trans também podem amamentar, caso utilizem hormônios!"
+            TextoUsuario2: "Você errou. Homens trans que possuem útero podem gerar uma criança, porém é necessário a interrupção do tratamento hormonal (testosterona). Podem até amamentar, caso não tenham passado por uma cirurgia de retirada das mamas (mastectomia). Mulheres trans também podem amamentar, caso utilizem hormônios!"
             },
             {
             Pergunta: "Qual o maior medo de pessoas trans no Brasil?",
@@ -67,7 +67,7 @@ class Transquiz {
             Resposta1: "Todas as opções acima",
             Resposta2: "Ir em médicos" || "Ser assassinado" || "Utilizar o banheiro" || "Não conseguir um emprego",
             TextoUsuario1: "Acertou. Que bom que você tem a consciência disso!",
-            TextoUsuario2: "Ser trans no Brasil envolve muitos medos. Parece até piada dizer que uma coisa simples como ir ao banheiro é motivo para sentir medo, porém é a verdade. É onde podemos sofrer agressão caso uma pessoa errada entre."
+            TextoUsuario2: "Você errou. Ser trans no Brasil envolve muitos medos. Parece até piada dizer que uma coisa simples como ir ao banheiro é motivo para sentir medo, porém é a verdade. É onde podemos sofrer agressão caso a pessoa errada entre no banheiro."
             },
             {
             Pergunta: "Qual a expectativa de vida de uma pessoa trans?",
@@ -79,7 +79,7 @@ class Transquiz {
             Resposta1: "35 anos",
             Resposta2: "86 anos" || "68 anos" || "21 anos" || "16 anos",
             TextoUsuario1: "Você acertou. A expectativa de vida é assustadoramente baixa, não é? Vamos mudar isso juntos?",
-            TextoUsuario2: "Infelizmente a expectativa de vida de uma pessoa trans é de 35 anos. Isso devido à assassinatos, ou suicídio. Que tal mudarmos isso juntos? Espalhe informação."
+            TextoUsuario2: "Você errou. Infelizmente a expectativa de vida de uma pessoa trans é de 35 anos. Isso devido à assassinatos, ou suicídio. Que tal mudarmos isso juntos? Espalhe informação."
             }
         ]
         this.questionSelected = this.getQuestion()
